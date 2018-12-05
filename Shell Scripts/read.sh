fd20cdc5586131d9986b4dfa1d6554d174465a63
Aditya Singh <outforaditya@gmail.com>
2018-12-06 01:09:23 +0530

    Read command with -p flag

--- Shell Scripts/read.sh
+++ Shell Scripts/read.sh
@@ -2,4 +2,10 @@
 
 echo "Enter name: "
 read name1 name2 name3
-echo "Enterd name: $name1 $name2 $nqame3"
+echo "usernam: $name1 $name2 $nqame3"
+
+
+# Read command with -p flag
+
+read -p 'username : ' username
+echo "username: $username"
